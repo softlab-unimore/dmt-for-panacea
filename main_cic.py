24437ba7d5e78c0083a62025481c39331af9e875
sarapeddy <269362@studenti.unimore.it>
2025-02-25 10:53:19 +0100

    add preprocessing for inf and nan values

--- main_cic.py
+++ main_cic.py
@@ -1,9 +1,16 @@
+from argparse import ArgumentParser
+
 import pandas as pd
-from DecisionTree import DecisionTree
+from numpy.ma.core import remainder
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+
+from decision_tree import DecisionTree
 import warnings
 from sklearn.exceptions import ConvergenceWarning
 from pandas.errors import SettingWithCopyWarning
-from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import OrdinalEncoder, FunctionTransformer
 from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,57 +18,86 @@ import numpy as np
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 
-# df = pd.read_csv('CICDATASET/Train_data.csv', delimiter=',')
-df = pd.read_csv('datasets/cicids.csv', delimiter=',')
-# ordinal_categories = ['protocol_type', 'service', 'flag']
-ordinal_categories = [col for col in df.columns if df[col].dtype == 'object']
-print('Ordinal Categories: ', ordinal_categories)
+def process_timestamps(df):
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%d/%m/%Y %H:%M:%S')
 
-# df_filter = df.drop(['protocol_type','service','flag'], axis=1)
-# Create and fit encoder
-df_filter = df.copy(deep=True)
-encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
-encoder.fit(df_filter[ordinal_categories])
-encoded_values = encoder.transform(df_filter[ordinal_categories])
-df_filter[ordinal_categories] = encoded_values
-original_values = encoder.inverse_transform(df_filter[ordinal_categories])
-# df_filter['class'] = df_filter['class'].map({'normal': 0, 'anomaly': 1})
-# df_filter['Label'] = df_filter['Label'].map({'Benign': 0}).fillna(1)
-print(len(df_filter))
+    date_cols = {
+        'year': df['Timestamp'].dt.year,
+        'month': df['Timestamp'].dt.month,
+        'day': df['Timestamp'].dt.day,
+        'hour': df['Timestamp'].dt.hour,
+        'minute': df['Timestamp'].dt.minute,
+        'weekday': df['Timestamp'].dt.weekday
+    }
 
-# TODO: FARE CATEGORICI
-# tree = DecisionTree(max_depth=100, min_points_per_leaf=30, closest_k_points=0.2, closer_DBSCAN_point=0.8, eps_DBSCAN=0.5)
-tree = DecisionTree(max_depth=200, min_points_per_leaf=20, closest_k_points=0.1, closer_DBSCAN_point=0.1, eps_DBSCAN=0.1, number_thresholds=2, ordinal_categories=ordinal_categories)
-n_iterations = 5
-n_train = 200
-n_test = len(df_filter) - (n_train*n_iterations) - 1
+    df = pd.concat([pd.DataFrame(date_cols), df.drop(columns=['Timestamp'])], axis=1)
+    return df
 
-for i in range(n_iterations):
-    print('Iteration: ', i)
+def replace_inf(x):
+    x = np.where(np.isinf(x), np.finfo(np.float64).max, x)
+    x = np.where(np.isneginf(x), np.finfo(np.float64).min, x)
+    return x
 
-    start_train = i * n_train
-    end_train = start_train + n_train
+if __name__=='__main__':
+    args = ArgumentParser()
+    args.add_argument('--dataset', type=str, default='cicids')
+    args.add_argument('--n_iterations', type=int, default=5)
+    args.add_argument('--n_train', type=int, default=200)
+    args.add_argument('--max_depth', type=int, default=200)
+    args.add_argument('--min_points_per_leaf', type=int, default=20)
+    args.add_argument('--closest_k_points', type=float, default=0.1)
+    args.add_argument('--closer_DBSCAN_point', type=float, default=0.1)
+    args.add_argument('--eps_DBSCAN', type=float, default=0.1)
+    args.add_argument('--number_thresholds', type=int, default=2)
+    args = args.parse_args()
 
-    # data_train, labels_train = df_filter.iloc[:-1,:-1][start_train:end_train], df_filter['class'][start_train:end_train]
-    data_train, labels_train = df_filter.iloc[:-1,:-1][start_train:end_train], df_filter['Label'][start_train:end_train]
+    df = pd.read_csv(f'datasets/{args.dataset}.csv', delimiter=',')
+    df = process_timestamps(df)
 
+    categorical_categories = [i for i, col in enumerate(df.columns) if df[col].dtype == 'object']
+    print('Ordinal Categories: ', categorical_categories)
 
-    if i == 0:
-        root = tree.partial_fit(None, data_train, labels_train)
-    else:
-        root = tree.partial_fit(root, data_train, labels_train)
+    ordinal_categories = [i for i, col in enumerate(df.columns) if i not in categorical_categories]
+    print('Ordinal Categories: ', ordinal_categories)
 
-    if i == n_iterations - 1:
-        start_test = end_train
-        end_test = start_test + n_test
-        data_test, labels_test = df_filter.iloc[:-1, :-1][start_test:end_test].copy(deep=True), df_filter['Label'][start_test:end_test].copy(deep=True)
+    pipeline = ColumnTransformer([
+        ('numerical', Pipeline([
+            ('replace_inf', FunctionTransformer(replace_inf)),
+            ('imputer', SimpleImputer(strategy='median'))
+        ]), ordinal_categories),
+        ('categorical', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1, dtype=int), categorical_categories)
+    ], remainder='passthrough')
 
-        get_anomalies = tree.detect_anomalies(root, data_test)
-        print(f1_score(labels_test, get_anomalies.sort_index()['Label'], average='macro'))
-        differences = sum(1 if x != y else 0 for x, y in zip(labels_test, get_anomalies.sort_index()['Label']))
-        print(differences, len(labels_test))
+    df_filter = pd.DataFrame(pipeline.fit_transform(df), columns=df.columns)
 
-   # print(get_anomalies)
+    tree = DecisionTree(max_depth=200, min_points_per_leaf=20, closest_k_points=0.1, closer_DBSCAN_point=0.1, eps_DBSCAN=0.1, number_thresholds=2, ordinal_categories=ordinal_categories)
 
-# Create the figure and first axis
-# print(f1_score(labels10, get_anomalies.sort_index()['Label'], average='macro'))
+    n_test = len(df_filter) - (args.n_train * args.n_iterations) - 1
+
+    for i in range(args.n_iterations):
+        print('Iteration: ', i)
+
+        start_train = i * args.n_train
+        end_train = start_train + args.n_train
+
+        data_train, labels_train = df_filter.iloc[:-1, :-1][start_train:end_train], df_filter['Label'][start_train:end_train]
+
+        if i == 0:
+            root = tree.partial_fit(None, data_train, labels_train)
+        else:
+            root = tree.partial_fit(root, data_train, labels_train)
+
+        if i == args.n_iterations - 1:
+            start_test = end_train
+            end_test = start_test + n_test
+            data_test, labels_test = df_filter.iloc[:-1, :-1][start_test:end_test].copy(deep=True), df_filter['Label'][start_test:end_test].copy(deep=True)
+
+            get_anomalies = tree.detect_anomalies(root, data_test)
+            print(f1_score(labels_test, get_anomalies.sort_index()['Label'], average='macro'))
+            differences = sum(1 if x != y else 0 for x, y in zip(labels_test, get_anomalies.sort_index()['Label']))
+            print(differences, len(labels_test))
+
+    # print(get_anomalies)
+
+    # Create the figure and first axis
+    # print(f1_score(labels10, get_anomalies.sort_index()['Label'], average='macro'))
