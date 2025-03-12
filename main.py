e4f1cb5b095ebe95841b8e36c4e7d4cc758307af
sarapeddy <269362@studenti.unimore.it>
2025-03-12 15:42:12 +0100

    Add function to load the cicids_improved dataset

--- main.py
+++ main.py
@@ -22,20 +22,6 @@ import numpy as np
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 
-def process_timestamps(df):
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%d/%m/%Y %H:%M:%S')
-
-    date_cols = {
-        'year': df['Timestamp'].dt.year,
-        'month': df['Timestamp'].dt.month,
-        'day': df['Timestamp'].dt.day,
-        'hour': df['Timestamp'].dt.hour,
-        'minute': df['Timestamp'].dt.minute,
-        'weekday': df['Timestamp'].dt.weekday
-    }
-
-    df = pd.concat([pd.DataFrame(date_cols), df.drop(columns=['Timestamp'])], axis=1)
-    return df
 
 def replace_inf(x):
     x = np.where(np.isinf(x), np.finfo(np.float64).max, x)
@@ -44,8 +30,7 @@ def replace_inf(x):
 
 if __name__=='__main__':
     args = ArgumentParser()
-    args.add_argument('--dataset', type=str, default='CICIDS2017')
-    args.add_argument('--train_perc', type=float, default=0.01)
+    args.add_argument('--dataset', type=str, default='CICIDS2017_improved')
     args.add_argument('--batch_size', type=int, default=200)
     args.add_argument('--max_depth', type=int, default=200)
     args.add_argument('--min_points_per_leaf', type=int, default=20)
@@ -56,17 +41,21 @@ if __name__=='__main__':
     args = args.parse_args()
 
     if args.dataset == 'CICIDS2017':
-        df = pd.read_csv(f'datasets/{args.dataset}.csv', delimiter=',')
-        df = process_timestamps(df)
+        df_train, df_test = load_dataset.load_cicids_2017(args.dataset)
     elif args.dataset == 'CICIDS2017_improved':
-        df = load_dataset.load_cicids_2017_improved(args.dataset)
+        df_train, df_test = load_dataset.load_cicids_2017_improved(args.dataset)
+    elif args.dataset == 'CICIDS2017_prova':
+        df = pd.read_csv(f'datasets/{args.dataset}.csv', delimiter=',')
+        df = load_dataset.process_timestamps(df)
+        train_end = int(len(df) * 0.2)
+        df_train, df_test = df.iloc[:train_end], df.iloc[train_end:]
     else:
         raise ValueError(f'Unknown dataset: {args.dataset}')
 
-    categorical_categories = [i for i, col in enumerate(df.columns) if df[col].dtype == 'object']
+    categorical_categories = [i for i, col in enumerate(df_train.columns) if df_train[col].dtype == 'object']
     print('Ordinal Categories: ', len(categorical_categories))
 
-    ordinal_categories = [i for i, col in enumerate(df.columns) if i not in categorical_categories]
+    ordinal_categories = [i for i, col in enumerate(df_train.columns) if i not in categorical_categories]
     print('Ordinal Categories: ', len(ordinal_categories))
 
     pipeline = ColumnTransformer([
@@ -78,15 +67,12 @@ if __name__=='__main__':
         ('categorical', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1, dtype=int), categorical_categories)
     ], remainder='passthrough')
 
-    df_filter = pd.DataFrame(pipeline.fit_transform(df), columns=df.columns)
-    dir_path = f'results/T{args.train_perc}B{args.batch_size}/run_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+    df_train = pd.DataFrame(pipeline.fit_transform(df_train), columns=df_train.columns)
+    df_test = pd.DataFrame(pipeline.transform(df_test), columns=df_test.columns)
+    dir_path = f'results/B{args.batch_size}/{args.dataset}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
     os.makedirs(dir_path, exist_ok=True)
 
     tree = DecisionTree(max_depth=200, min_points_per_leaf=20, closest_k_points=0.1, closer_DBSCAN_point=0.1, eps_DBSCAN=0.1, number_thresholds=2, ordinal_categories=ordinal_categories)
-
-    train_end = int(len(df_filter) * args.train_perc)
-    if train_end % args.batch_size != 0:
-        train_end = train_end + (train_end % args.batch_size)
 
     total_time = time.time()
 
@@ -94,9 +80,12 @@ if __name__=='__main__':
     print('Start Train')
     print('------------------------------------')
 
-    for i in tqdm(range(0, train_end, args.batch_size)):
+    if df_train.shape[0] % args.batch_size != 0:
+        df_train = df_train.iloc[:-(df_train.shape[0] % args.batch_size)]
 
-        data_train, labels_train = df_filter.iloc[:, :-1][i:i+args.batch_size], df_filter['Label'][i:i+args.batch_size]
+    for i in tqdm(range(0, df_train.shape[0], args.batch_size)):
+
+        data_train, labels_train = df_train.iloc[:, :-1][i:i+args.batch_size], df_train['Label'][i:i+args.batch_size]
 
         if i == 0:
             root = tree.partial_fit(None, data_train, labels_train)
@@ -108,7 +97,7 @@ if __name__=='__main__':
     print('Start Test')
     print('------------------------------------')
 
-    data_test, labels_test = df_filter.iloc[train_end:, :-1].copy(deep=True), df_filter.loc[train_end:, 'Label'].copy(deep=True)
+    data_test, labels_test = df_test.iloc[:, :-1].copy(deep=True), df_test.loc[:, 'Label'].copy(deep=True)
 
     test_time = time.time()
     # get_anomalies = tree.detect_anomalies(root, data_test)
