1c37d08c75ae49ba5d3918a90918fe5523fb3f0e
sarapeddy <269362@studenti.unimore.it>
2025-03-18 10:41:59 +0100

    Add the time preprocessing for cicids2017_improved

--- load_dataset.py
+++ load_dataset.py
@@ -20,8 +20,8 @@ def load_cicids_2017_improved(dataset):
     test_files = ['wednesday.csv', 'thursday.csv', 'friday.csv']
     df_train = pd.concat([pd.read_csv(f'./datasets/{dataset}/{file}', delimiter=',') for file in train_files], axis=0)
     df_test = pd.concat([pd.read_csv(f'./datasets/{dataset}/{file}', delimiter=',') for file in test_files], axis=0)
-    df_train = df_train.drop(columns=['id', 'Flow ID', 'Src IP', 'Dst IP', 'Src Port', 'Timestamp'])
-    df_test = df_test.drop(columns=['id', 'Flow ID', 'Src IP', 'Dst IP', 'Src Port', 'Timestamp'])
+    df_train = process_timestamps(df_train, format='%Y-%m-%d %H:%M:%S.%f')
+    df_test = process_timestamps(df_test, format='%Y-%m-%d %H:%M:%S.%f')
     return df_train.iloc[:, :-1], df_test.iloc[:, :-1]
 
 def load_csv(dataset, test_file='TestData.csv'):
