d6da94ee85e6d4627eb2acbbc6389959a736afa6
sarapeddy <269362@studenti.unimore.it>
2025-05-13 15:36:36 +0200

    add the delay and the supervision during the testing phase

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from datetime import datetime
 
 from argparse import ArgumentParser
 
+import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.manifold import TSNE
 
 from UnsupervisedDMT.decision_tree import DecisionTree
 import warnings
 from sklearn.exceptions import ConvergenceWarning
+from sklearn.decomposition import PCA
 from pandas.errors import SettingWithCopyWarning
 from data_utils.load_dataset import get_dataset
 
@@ -28,7 +32,7 @@ def get_args():
     args = ArgumentParser()
     args.add_argument('--dataset', type=str, default='Kitsune')
     args.add_argument('--batch_size', type=int, default=1024)
-    args.add_argument('--mode', type=str, default='')
+    args.add_argument('--mode', type=str, default='with_pca')
     args.add_argument('--max_depth', type=int, default=20)
     args.add_argument('--dist_threshold', type=float, default=0.25)
     args.add_argument('--homogeneity_gain_threshold', type=float, default=0)
@@ -36,6 +40,8 @@ def get_args():
     args.add_argument('--min_points_per_leaf', type=int, default=20)
     args.add_argument('--closest_k_points', type=float, default=0.1)
     args.add_argument('--number_thresholds', type=int, default=2)
+    args.add_argument('--pca', action='store_true', default=False)
+    args.add_argument('--delay', action='store_true', default=False)
     args = args.parse_args()
 
     return args
@@ -44,6 +50,23 @@ if __name__=='__main__':
     args = get_args()
     df_train, df_test = get_dataset(args)
     df_train, df_test, cat = preprocess(df_train, df_test)
+
+    print(f'Dataset shape: {df_train.shape}')
+
+    if args.pca:
+        # Apply PCA with 0.95 variance ratio
+        pca = PCA(n_components=0.95)
+        df_train_pca = pca.fit_transform(df_train.iloc[:, :-1])
+        df_test_pca = pca.transform(df_test.iloc[:, :-1])
+
+        df_train = np.concatenate((df_train_pca, df_train.iloc[:, -1].values.reshape(-1, 1)), axis=1)
+        df_test = np.concatenate((df_test_pca, df_test.iloc[:, -1].values.reshape(-1, 1)), axis=1)
+
+        df_train = pd.DataFrame(df_train, columns=[f'PC{i+1}' for i in range(df_train_pca.shape[1])] + ['Label'])
+        df_test = pd.DataFrame(df_test, columns=[f'PC{i+1}' for i in range(df_train_pca.shape[1])] + ['Label'])
+
+    print(f"PCA shape: {df_train.shape}")
+
     dir_path = f'results/B{args.batch_size}/{args.mode}/{args.dataset}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
     os.makedirs(dir_path, exist_ok=True)
     save_parameters(args, dir_path)
@@ -92,17 +115,27 @@ if __name__=='__main__':
     for i in range(0, df_test.shape[0], args.batch_size):
         print(f'Batch {i} / {df_test.shape[0]}')
         data_test, labels_test = df_test.iloc[:, :-1][i:i + args.batch_size], df_test['Label'][i:i + args.batch_size]
-        batch_results = tree.detect_anomalies_with_mad(root, data_test, labels_test, T_mad=args.tmad)
+        # batch_results = tree.detect_anomalies_with_mad(root, data_test, labels_test, T_mad=args.tmad)
+        # batch_results = tree.detect_anomalies_with_mean(root, data_test, labels_test, T_mad=args.tmad)
+        batch_results = tree.detect_anomalies_with_adaptive_clusters(root, data_test, labels_test)
         results = pd.concat([results, batch_results], axis=0)
 
         if i % 40 == 0:
             batch_time = time.time() - test_time
             metrics = get_metrics(results, batch_time, total_time)
 
+        if args.delay:
+            i = i - args.batch_size * 10
+
+        if i >= 0:
+            print('Update tree...')
+            data_train, labels_train = df_test.iloc[:, :-1][i: i + args.batch_size], df_test['Label'][i: i + args.batch_size]
+            root = tree.partial_fit(root, data_train, labels_train)
+
     results.to_csv(f'{dir_path}/results_{args.dataset}_batch.csv', index=False)
     test_time = time.time() - test_time
 
     metrics = get_metrics(results, test_time, total_time)
-    save_metrics(metrics, dir_path)
+    save_metrics(metrics, dir_path, conf=f'tmad_{args.tmad}')
 
     print('Finish')
