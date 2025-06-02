69215f897bebc56103ae0314d8f05379ef69d895
MICHELE LUCA CONTALBO <n334698@fg-s1.ing.unimo.it>
2025-06-02 11:53:07 +0200

    added semi-supervised training to DMT

--- dmt_tester.py
+++ dmt_tester.py
@@ -7,6 +7,7 @@ from data_utils.preprocessing import preprocess
 from datetime import datetime
 from argparse import ArgumentParser
 from typing import Optional
+from sklearn.cluster import KMeans
 
 import time
 import os
@@ -26,15 +27,75 @@ def get_args():
     args = ArgumentParser()
     args.add_argument('--dataset', type=str, default='CICIDS2017')
     args.add_argument('--batch_size', type=int, default=None)
+    args.add_argument('--delay', type=int, default=10)
+    args.add_argument('--sampling', type=float, default=0.1)
     args = args.parse_args()
 
     return args
 
+# copy of corresponding function in main.py
+def find_best_kmeans(data_train):
+    list_kmeans = []
+    distorsions = []
+
+    for k in range(1, 7):
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans.fit(data_train)
+        list_kmeans.append(kmeans)
+        distorsions.append(kmeans.inertia_)
+
+    first_derivative = np.diff(distorsions)
+    optimal_k = np.argmin(first_derivative) + 2
+
+    try:
+        kmeans = list_kmeans[optimal_k]
+    except IndexError:
+        kmeans = list_kmeans[-1]
+
+    return kmeans
+
+# copy of corresponding function in main.py
+def find_data_sampled(data_train, labels_train, kmeans, sampling_perc):
+    data_train_sampled_list = []
+    labels_train_sampled_list = []
+
+    for i in np.unique(kmeans.labels_):
+        data_cluster = data_train[kmeans.labels_ == i]
+        labels_cluster = labels_train[kmeans.labels_ == i]
+
+        mask = np.random.rand(len(data_cluster)) < sampling_perc
+
+        data_sample = data_cluster[mask]
+        labels_sample = labels_cluster[mask]
+
+        if len(data_sample) > 0: #not data_sample.empty:
+            data_train_sampled_list.append(data_sample)
+        if len(labels_sample) > 0: #not labels_sample.empty:
+            labels_train_sampled_list.append(labels_sample)
+
+    # data_train_sampled = pd.concat(data_train_sampled_list, axis=0) if data_train_sampled_list else pd.DataFrame(columns=data_train.columns)
+    # labels_train_sampled = pd.concat(labels_train_sampled_list, axis=0) if labels_train_sampled_list else pd.Series(name=labels_train.name)
+    if data_train_sampled_list:
+        data_train_sampled = np.concatenate(data_train_sampled_list, axis=0)
+    else:
+        data_train_sampled = np.empty((0, data_train.shape[1]))
+
+    if labels_train_sampled_list:
+        labels_train_sampled = np.concatenate(labels_train_sampled_list, axis=0)
+    else:
+        labels_train_sampled = np.empty((0,), dtype=labels_train.dtype)
+
+    return data_train_sampled, labels_train_sampled
+
 class DMTRunner:
-    def __init__(self, params: dict, dataset_name: str, batch_size: Optional[int] = None):
+    def __init__(self, params: dict, dataset_name: str, batch_size: Optional[int] = None, sampling: Optional[float] = 0, delay: Optional[int] = 0):
+        # sampling 0 -> no online training; sampling 1 -> full test set is used for training
+        # delay: number of batches to wait before training on test set
         self.model = DynamicModelTree(**params)
         self.stream = None
         self.batch_size = batch_size
+        self.sampling = sampling
+        self.delay = delay
         self.dataset_name = dataset_name
         self.dir_path = None
 
@@ -58,7 +119,7 @@ class DMTRunner:
             if self.batch_size is None:
                 self.batch_size = self.calculate_batch_size(csv_path)
             print(f"Batch size chosen: {self.batch_size}")
-            self.dir_path = f'./results/DMT/B{self.batch_size}/{self.dataset_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
+            self.dir_path = f'./results/DMT/B{self.batch_size}/{self.sampling}/{self.delay}/{self.dataset_name}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
             os.makedirs(self.dir_path, exist_ok=True)
 
             stream = FileStream(csv_path, target_idx=-1)
@@ -79,7 +140,7 @@ class DMTRunner:
             if verbose:
                 if iteration != 0 and iteration % 50 == 0:
                     print(f"Reached iteration {iteration}")
-                iteration += 1
+            iteration += 1
 
         self.train_stream.restart()
         print("Training finished. Saving the model...")
@@ -91,6 +152,7 @@ class DMTRunner:
         targets = np.array([], dtype=np.int32)
         iteration = 0
         print("Start testing...")
+        feature_batches, target_batches = [], []
         while self.test_stream.has_more_samples():
             x, y = self.test_stream.next_sample(batch_size=self.batch_size)
             y_pred = self.model.predict(x)
@@ -98,12 +160,23 @@ class DMTRunner:
             preds = np.append(preds, y_pred)
             targets = np.append(targets, y.astype(np.int32))
 
-            # self.model.partial_fit(x, y) #DMT trains online
+            feature_batches.append(x)
+            feature_batches = feature_batches[-self.delay-1:] #code to prevent testing to completely fill the CPU. The -1 is added just to be sure not to remove meaningful batches
+            target_batches.append(y)
+            target_batches = target_batches[-self.delay-1:]
+
+            num_batch_to_train = iteration - self.delay
+            if num_batch_to_train >= 0:
+                if self.sampling > 0:
+                    # print("Update tree...")
+                    kmeans = find_best_kmeans(feature_batches[-self.delay])
+                    data_train, labels_train = find_data_sampled(feature_batches[-self.delay], target_batches[-self.delay], kmeans, args.sampling)
+                    self.model.partial_fit(data_train, labels_train)
 
             if verbose:
                 if iteration != 0 and iteration % 50 == 0:
                     print(f"Reached iteration {iteration}")
-                iteration += 1
+            iteration += 1
 
         print("Testing finished. Saving the model...")
         self.save_model(name="tree_test.pkl")
@@ -136,7 +209,7 @@ if __name__ == "__main__":
         "cat_features": None, # TODO: detect categorical features and put them here
     }
 
-    dm = DMTRunner(params, args.dataset, args.batch_size)
+    dm = DMTRunner(params, args.dataset, args.batch_size, args.sampling, args.delay)
     dm.load_stream(train_path)
     dm.load_stream(test_path, stream_type="test")
 
--- main.py
+++ main.py
@@ -1,31 +1,91 @@
+import json
 import os
 import pickle
 import time
 from datetime import datetime
 
 from argparse import ArgumentParser
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn.manifold import TSNE
+
 from UnsupervisedDMT.decision_tree import DecisionTree
 import warnings
 from sklearn.exceptions import ConvergenceWarning
+from sklearn.decomposition import PCA
 from pandas.errors import SettingWithCopyWarning
 from data_utils.load_dataset import get_dataset
 
 from data_utils.preprocessing import preprocess
-from util import get_metrics, save_metrics
+from util import get_metrics, save_metrics, save_parameters
 
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
 warnings.filterwarnings("ignore", category=SettingWithCopyWarning)
 
+
+def find_best_kmeans(data_train):
+    list_kmeans = []
+    distorsions = []
+
+    for k in range(1, 7):
+        kmeans = KMeans(n_clusters=k, random_state=42)
+        kmeans.fit(data_train)
+        list_kmeans.append(kmeans)
+        distorsions.append(kmeans.inertia_)
+
+    first_derivative = np.diff(distorsions)
+    optimal_k = np.argmin(first_derivative) + 2
+
+    try:
+        kmeans = list_kmeans[optimal_k]
+    except IndexError:
+        kmeans = list_kmeans[-1]
+
+    return kmeans
+
+
+def find_data_sampled(data_train, labels_train, kmeans, sampling_perc):
+    data_train_sampled_list = []
+    labels_train_sampled_list = []
+
+    for i in np.unique(kmeans.labels_):
+        data_cluster = data_train[kmeans.labels_ == i]
+        labels_cluster = labels_train[kmeans.labels_ == i]
+
+        mask = np.random.rand(len(data_cluster)) < sampling_perc
+
+        data_sample = data_cluster[mask]
+        labels_sample = labels_cluster[mask]
+
+        if not data_sample.empty:
+            data_train_sampled_list.append(data_sample)
+        if not labels_sample.empty:
+            labels_train_sampled_list.append(labels_sample)
+
+    data_train_sampled = pd.concat(data_train_sampled_list, axis=0) if data_train_sampled_list else pd.DataFrame(columns=data_train.columns)
+    labels_train_sampled = pd.concat(labels_train_sampled_list, axis=0) if labels_train_sampled_list else pd.Series(name=labels_train.name)
+
+    return data_train_sampled, labels_train_sampled
+
+
 def get_args():
     args = ArgumentParser()
-    args.add_argument('--dataset', type=str, default='CICIDS2017')
+    args.add_argument('--dataset', type=str, default='Kitsune')
     args.add_argument('--batch_size', type=int, default=1024)
-    args.add_argument('--mode', type=str, default='')
+    args.add_argument('--mode', type=str, default='with_pca')
     args.add_argument('--max_depth', type=int, default=20)
     args.add_argument('--dist_threshold', type=float, default=0.25)
+    args.add_argument('--homogeneity_gain_threshold', type=float, default=0)
+    args.add_argument('--tmad', type=float, default=3.5)
     args.add_argument('--min_points_per_leaf', type=int, default=20)
     args.add_argument('--closest_k_points', type=float, default=0.1)
     args.add_argument('--number_thresholds', type=int, default=2)
+    args.add_argument('--pca', action='store_true', default=False)
+    args.add_argument('--delay', type=int, default=10)
+    args.add_argument('--sampling', type=float, default=0.1)
     args = args.parse_args()
 
     return args
@@ -34,10 +94,36 @@ if __name__=='__main__':
     args = get_args()
     df_train, df_test = get_dataset(args)
     df_train, df_test, cat = preprocess(df_train, df_test)
+
+    print(f'Dataset shape: {df_train.shape}')
+
+    if args.pca:
+        # Apply PCA with 0.95 variance ratio
+        pca = PCA(n_components=0.95)
+        df_train_pca = pca.fit_transform(df_train.iloc[:, :-1])
+        df_test_pca = pca.transform(df_test.iloc[:, :-1])
+
+        df_train = np.concatenate((df_train_pca, df_train.iloc[:, -1].values.reshape(-1, 1)), axis=1)
+        df_test = np.concatenate((df_test_pca, df_test.iloc[:, -1].values.reshape(-1, 1)), axis=1)
+
+        df_train = pd.DataFrame(df_train, columns=[f'PC{i+1}' for i in range(df_train_pca.shape[1])] + ['Label'])
+        df_test = pd.DataFrame(df_test, columns=[f'PC{i+1}' for i in range(df_train_pca.shape[1])] + ['Label'])
+
+    print(f"PCA shape: {df_train.shape}")
+
     dir_path = f'results/B{args.batch_size}/{args.mode}/{args.dataset}_{datetime.now().strftime("%Y%m%d_%H%M%S")}'
     os.makedirs(dir_path, exist_ok=True)
+    save_parameters(args, dir_path)
 
-    tree = DecisionTree(max_depth=args.max_depth, min_points_per_leaf=args.min_points_per_leaf, dist_threshold=args.dist_threshold, closest_k_points=0.1, closer_DBSCAN_point=0.1, eps_DBSCAN=0.1, number_thresholds=2, ordinal_categories=cat['ordinal_categories'])
+    tree = DecisionTree(
+        max_depth=args.max_depth,
+        min_points_per_leaf=args.min_points_per_leaf,
+        dist_threshold=args.dist_threshold,
+        homogeneity_gain_threshold=args.homogeneity_gain_threshold,
+        closest_k_points=args.closest_k_points,
+        number_thresholds=args.number_thresholds,
+        ordinal_categories=cat['ordinal_categories']
+    )
 
     total_time = time.time()
 
@@ -64,15 +150,40 @@ if __name__=='__main__':
     print('Start Test')
     print('------------------------------------')
 
-    data_test, labels_test = df_test.iloc[:, :-1].copy(deep=True), df_test.loc[:, 'Label'].copy(deep=True)
-
     test_time = time.time()
-    # get_anomalies = tree.detect_anomalies(root, data_test)
-    results = tree.detect_anomalies_with_mad(root, data_test, labels_test)
-    results.to_csv(f'{dir_path}/results_{args.dataset}.csv', index=False)
+
+    if df_test.shape[0] % args.batch_size != 0:
+        df_test = df_test.iloc[:-(df_test.shape[0] % args.batch_size)]
+
+    results = pd.DataFrame(columns=['Label', 'Predicted'])
+    for i in range(0, df_test.shape[0], args.batch_size):
+        print(f'Batch {i} / {df_test.shape[0]}')
+        data_test, labels_test = df_test.iloc[:, :-1][i:i + args.batch_size], df_test['Label'][i:i + args.batch_size]
+        # batch_results = tree.detect_anomalies_with_mad(root, data_test, labels_test, T_mad=args.tmad)
+        # batch_results = tree.detect_anomalies_with_mean(root, data_test, labels_test, T_mad=args.tmad)
+        batch_results = tree.detect_anomalies_with_adaptive_clusters(root, data_test, labels_test)
+        results = pd.concat([results, batch_results], axis=0)
+
+        if i % 40 == 0:
+            batch_time = time.time() - test_time
+            metrics = get_metrics(results, batch_time, total_time)
+
+        i = i - args.batch_size * args.delay
+
+        if i >= 0:
+            print('Update tree...')
+            data_train, labels_train = df_test.iloc[:, :-1][i: i + args.batch_size], df_test['Label'][i: i + args.batch_size]
+
+            if args.sampling > 0:
+                kmeans = find_best_kmeans(data_train)
+                data_train, labels_train = find_data_sampled(data_train, labels_train, kmeans, args.sampling)
+
+            root = tree.partial_fit(root, data_train, labels_train)
+
+    results.to_csv(f'{dir_path}/results_{args.dataset}_batch.csv', index=False)
     test_time = time.time() - test_time
 
     metrics = get_metrics(results, test_time, total_time)
-    save_metrics(metrics, dir_path)
+    save_metrics(metrics, dir_path, conf=f'tmad_{args.tmad}')
 
     print('Finish')
